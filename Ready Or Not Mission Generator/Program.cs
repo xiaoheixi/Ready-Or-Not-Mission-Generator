3c32def72fcb1967bc11eec66d8544cd78b164b1
xiaoheixi <justinzhao893@gmail.com>
2022-10-24 17:30:52 +1100

    First commit!

--- Ready Or Not Mission Generator/Program.cs
+++ Ready Or Not Mission Generator/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] missionArray = { "213 PARK HOMES", "4U GAS STATION", "BRISA COVE", "CAESAR'S CARS DEALERSHIP", "CHERRYESSA FARM", "CLUB", "FAST FOOD", "HOTEL ROOMS", "PORT HOKEN", "VALLEY OF THE DOLLS", "WENDERLY HILLS HOTEL" };
+Console.WriteLine("Welcome to Ready Or Not Mission Generator by Justin Zhao!");
+Console.WriteLine("Press 1 to generate a mission.");
+Console.WriteLine("Press 0 to exit.");
+string input = Console.ReadLine();
+while(Convert.ToInt32(input) != 0)
+{
+    Random rnd = new Random();
+    int mission = rnd.Next(0, 10);
+    if (mission == 0)
+    {
+        Console.WriteLine(missionArray[mission]);
+        int type = rnd.Next(1, 2);
+        Console.WriteLine(type);
+    }
+}
+Console.WriteLine("Thanks for using Ready Or Not Mission Generator by Justin Zhao!");
