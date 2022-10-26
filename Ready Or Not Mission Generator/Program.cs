5aa095e3c5413f7bf24ad43f88e88ccb4fcc3c0e
xiaoheixi <justinzhao893@gmail.com>
2022-10-26 12:48:12 +1100

    All missions added!

--- Ready Or Not Mission Generator/Program.cs
+++ Ready Or Not Mission Generator/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] missionArray = { "213 PARK HOMES", "4U GAS STATION", "BRISA COVE", "CAESAR'S CARS DEALERSHIP", "CHERRYESSA FARM", "CLUB", "FAST FOOD", "HOTEL ROOMS", "PORT HOKEN", "VALLEY OF THE DOLLS", "WENDERLY HILLS HOTEL" };
+string[] missionArray = { "VOLL HEALTH HOUSE", "CHERRYESSA FARM", "BRISA COVE", "NEON NIGHTCLUB", "HOTEL ROOMS", "WENDERLY HILLS HOTEL", "4U GAS STATION", "TEST LEVEL: AGENCY", "CAESAR'S CARS DEALERSHIP", "PORT HOKAN", "TEST LEVEL: FAST FOOD", "213 PARK HOMES" };
 Console.WriteLine("Welcome to Ready Or Not Mission Generator by Justin Zhao!");
 Console.WriteLine("Press 1 to generate a mission.");
 Console.WriteLine("Press 0 to exit.");
@@ -7,14 +7,45 @@ string input = Console.ReadLine();
 while(Convert.ToInt32(input) != 0)
 {
     Random rnd = new Random();
-    int mission = rnd.Next(0, 10);
-    if (mission == 0)
+    int mission = rnd.Next(0, 11);
+    Console.WriteLine(missionArray[mission]);
+    if (mission == 1)
     {
-        Console.WriteLine(missionArray[mission]);
+        int type = rnd.Next(1, 2);
+        Console.WriteLine(type);
+    }
+    if (mission == 5)
+    {
+        int type = rnd.Next(1, 5);
+        Console.WriteLine(type);
+    }
+    if(mission == 6)
+    {
+        int type = rnd.Next(1, 5);
+        Console.WriteLine(type);
+    }
+    if(mission == 8)
+    {
+        int type = rnd.Next(1, 2);
+        Console.WriteLine(type);
+    }
+    if(mission == 9)
+    {
+        int type = rnd.Next(1, 4);
+        Console.WriteLine(type);
+    }
+    if(mission == 10)
+    {
+        int type = rnd.Next(1, 3);
+        Console.WriteLine(type);
+    }
+    if(mission == 11)
+    {
         int type = rnd.Next(1, 2);
         Console.WriteLine(type);
     }
     Console.WriteLine("Press 1 to generate a new mission.");
     Console.WriteLine("Press 0 to exit.");
+    input = Console.ReadLine();
 }
 Console.WriteLine("Thanks for using Ready Or Not Mission Generator by Justin Zhao!");
